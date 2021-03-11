adbe1511c5e218e4c0f3624ab2d4ccde370bc117
Bashu Tiwari <55925918+bashu22tiwari@users.noreply.github.com>
2021-03-11 21:27:23 +0530

    Time Complexity.

---
+++ CodeChef/Beginner/LADDU.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include <cstdlib>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int tt;
+    cin >> tt;
+    while(tt--)
+    {
+        int n;
+        cin >> n ;
+        string origin;
+        cin >> origin;
+        int count=0 ;
+
+    for(int i=0 ; i<n ; i++)
+    {
+        if(i==0)
+        {
+            string s;
+            cin >> s;
+            int rank;
+            cin >> rank;
+
+            if(rank)
+        }
+    }
+
+        
+        
+    }
+
+   
+
+}
+
+
+
+    
