7cc3d60f5f47f3a44e5215a2108834d014195985
Bashu Tiwari <55925918+bashu22tiwari@users.noreply.github.com>
2021-04-05 22:08:22 +0530

    Codechef.

---
+++ CodeChef/Beginner/SDICE.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <cstdlib>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <stack>
+
+int main(){
+    int tt;
+    cin >> tt ;
+    while(tt--){
+        int n;
+        cin >> n ;
+
+        if(n==1){
+            cout << "20\n";
+        }
+        else if(n==2){
+            cout << "40\n";
+        }
+        else if(n==3){
+            cout << "46\n";
+        }
+        else if(n==4){
+            cout << "48\n";
+        }
+    }
+}
