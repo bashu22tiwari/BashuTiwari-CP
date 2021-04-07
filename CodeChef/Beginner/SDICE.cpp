90d26580dc36a5b642ac0219594206dfabdf271d
Bashu Tiwari <55925918+bashu22tiwari@users.noreply.github.com>
2021-04-07 21:25:16 +0530

    Some Basic Questions.

--- CodeChef/Beginner/SDICE.cpp
+++ CodeChef/Beginner/SDICE.cpp
@@ -4,6 +4,7 @@
 #include <cstring>
 #include <vector>
 #include <stack>
+using namespace std;
 
 int main(){
     int tt;
@@ -16,13 +17,47 @@ int main(){
             cout << "20\n";
         }
         else if(n==2){
-            cout << "40\n";
+            cout << "36\n";
         }
         else if(n==3){
-            cout << "46\n";
+            cout << "51\n";
         }
         else if(n==4){
-            cout << "48\n";
+            cout << "60\n";
+        }
+        else if(n>=5){
+            int bottom_covered = n/4 - 1 ;
+            int pips_bottom_covered = bottom_covered*44;
+            int covered = n%4;
+            int not_covered , top_pips , pips_covered , pips_not_covered;
+
+            if(covered==0){
+                pips_not_covered =0;
+                top_pips=60;
+                pips_covered=0;
+            }
+            else
+            {
+                not_covered = 4-covered;
+                pips_covered = covered*11;
+                pips_not_covered = not_covered*15;
+                if(covered==1){
+                    top_pips = 20;
+                }
+                else if(covered==2){
+                    top_pips = 36;
+                }
+                else if(covered==3){
+                    top_pips = 51;
+                }
+                else if(covered==4){
+                    top_pips = 60;
+                }
+            }
+
+            cout << pips_bottom_covered+pips_covered+pips_not_covered+top_pips << "\n";
+
         }
     }
+    return 0;
 }
