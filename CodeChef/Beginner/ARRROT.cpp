091abbbb40d4456c210753cf78454377bd01467b
Bashu Tiwari <55925918+bashu22tiwari@users.noreply.github.com>
2021-05-01 21:58:37 +0530

    Binary Tree

--- CodeChef/Beginner/ARRROT.cpp
+++ CodeChef/Beginner/ARRROT.cpp
@@ -2,63 +2,24 @@
 using namespace std;
 
 
+
+
 int main(){
     int n;
-    cin >> n ;
-    int *arr = new int[n];
-    for(int i=0 ; i<n ; i+=){
+    cin >> n;
+    int arr[n];
+    for(int i=0 ; i<n i ++){
         cin >> arr[i];
     }
-
-    int queries_number;
-    cin >> queries_number;
-    int queries[queries_number];
-    for(int i=0 ; i<queries_number ; i++){
-        cin >> queries[n];
+    int sum = 0;
+    for(int i=0 ; i<n ; i++){
+        sum+=arr[i];
+    }
+    int q;
+    cin >> q;
+    for(int i=0 ; i<q ; i++){
+        sum*=2;
     }
 
-    for(int i=0 ; i<queries_number ; i++){
-        if(queries[i]>=0){
-            int x = queries[i];
-            int newn = n*2;
-            int *newarr = new int[newnn];
-            for(int i=0 ; i<n ; i++){
-                newarr[i]=arr[i];
-            }
-
-            int temp[x];
-            for(int i=0 ; i<x ; i+=){
-                temp[i] = arr[i];
-            }
-            for(int i=0 ; i<n-x ; i+=){
-                arr[i]=arr[i+x];
-            }
-            for(int i=n-x, int j=0 ; i<n ; i++){
-                arr[i]=temp[j++];
-            }
-
-            for(int i=n ; i<newn ; i++){
-                newarr[i]==arr[i-n];
-            }
-            n=n*2;
-            arr=newarr;  
-        }
-        else{
-            int x = abs(queries[i]);
-            int newn = n*2;
-            int *newarr = new int[newnn];
-            for(int i=0 ; i<n ; i++){
-                newarr[i]=arr[i];
-            }
-
-            int temp[]
-
-            for(int i=n ; i<newn ; i++){
-                newarr[i]==arr[i-n];
-            }
-            n=n*2;
-            arr=newarr;
-
-        }
-    }
+    cout << sum;
 }
--- xyz.cpp
+++ xyz.cpp
@@ -1,40 +1,27 @@
 #include <bits/stdc++.h>
-#include <cstdlib>
-#include <algorithm>
-#include <cstring>
+#define ll long long int
+#define M 1000000007
 using namespace std;
 
-int main()
-{
-    int t;
-    cin >> t ;
-    while(t--)
-    {
-        int n;
-        cin >> n ;
-        int d, max=0;
-        int array[n];
-        for(int i=0 ; i<n ; i++)
-        {
-            cin >> array[i];
-        }
-        for(int i=0 ; i<n-2 ; i++)
-        {
-            for (int j=1; j<n-1; j++)
-            {
-                for(int k=2 ; k<n ; k++)
-                {
-                    d = abs(array[i]-array[j]) + abs(array[j]-array[k]) + abs(array[i]-array[k]);
-                    if(d>max)
-                    {
-                        max=d;
-                    }
+int main() {
 
-                }
-            }
-            
-        }
-    cout << max << "\n" ;
+    ll n;
+    cin >> n;
+    ll a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
     }
-}
 
+    ll sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += a[i] ;
+    }
+
+    ll q;
+    cin >> q;
+    for (int i = 0; i < q; i++) {
+        sum = (sum * 2) % M;
+        cout << sum << endl;
+    }
+
+}
