9435b1e6ee6d535e950cc3819dc9ca35f4f4aef4
Bashu Tiwari <55925918+bashu22tiwari@users.noreply.github.com>
2021-05-04 23:21:03 +0530

    Recursion.

---
+++ Geeks For Geeks/palindrome.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPalindrome(string s, int start , int end){
+    
+    if(start>=end){
+        return true;
+    }
+
+    if(s[start]!=s[end]){
+        return false;
+    }
+
+    return (s,start+1,end-1);
+}
+
+int main(){
+    string s = "geeksaeeg";
+    int end  = s.size();
+    end--;
+    cout << isPalindrome(s ,0 ,end) ;
+}
--- xyz.cpp
+++ xyz.cpp
@@ -1,12 +1,30 @@
-    if(n%11==0){
-        cout << "Eleven";
-    }
-    else if(n%3==0){
-        cout << "Three";
-    }
-    else if(n%2==0){
-        cout << "Two";
-    }
-    else{
-        cout << "-1";
-    }
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int *p1 = new int[1];
+    int *p2 = new int[1];
+
+    p1[0] = 1;
+    // p1[1] = 1;
+    // p1[2] = 1;
+    // p1[3] = 1;
+    // p1[4] = 1;
+
+    
+    p2[0] = 1;
+    // p2[1] = 1;
+    // p2[2] = 1;
+    // p2[3] = 1;
+    // p2[4] = 1;
+
+    
+
+    // cout << p1 << " " << p2 << endl;
+    // cout << p1+1 << " " << p2+1 << endl;
+    // cout << *p1 << " " << *p2 << endl;
+    // cout << *p1+1 << " " << *p2+1 << endl;
+    // cout << *(p1+1) << " " << *(p2+1) << endl;
+    // cout << p1+2 << " " << *(p2+3) << endl;
+    cout << p1-p2 ;
+}
