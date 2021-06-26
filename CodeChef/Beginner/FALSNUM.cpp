e4cf43c3fc1026bb1ae3653225f9cd9e12d2bbd9
Bashu Tiwari <55925918+bashu22tiwari@users.noreply.github.com>
2021-06-26 23:03:25 +0530

    Contest Codechef.

---
+++ CodeChef/Beginner/FALSNUM.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tt;
+    cin >> tt;
+    while(tt--){
+        long long int a;
+        cin >> a ;
+        string str = to_string(a);
+        
+        // if(str[i]>1){
+        //     str = '1'+str;
+        //     cout << str;
+        // }
+        cout << str << endl;
+    }
+}
---
+++ CodeChef/Beginner/WAV2.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long int n,q;
+    cin >> n >> q ;
+    
+    long long int roots[n];
+    for(long long int i=0 ; i<n ; i++){
+        cin >> roots[i];
+    }
+
+    sort(roots,roots+n);
+
+    for(long long int i=0 ; i<q ; i++){
+        int given;
+        cin >> given;
+        
+    }
+}
