99c1d9ea4ce7cb6114508683635d62f04dbd64b8
Bashu Tiwari <bashutiwariapsg22@gmail.com>
2021-09-28 02:07:53 +0530

    Codechef Starter 13

--- abc.cpp
+++ abc.cpp
@@ -5,9 +5,37 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int tt;
+    long long int tt;
     cin >> tt;
     while(tt--){
-        
+        long long int a,b;
+        cin >> a >> b;
+        b = b*b;
+        if(a==b){
+            cout << "YES\n";
+        }
+        else if(a<b){
+            b = b*b;
+            if(b%a==0){
+                cout << "YES\n";
+            }
+            else{
+                cout << "NO\n";
+            }
+        }
+        else{
+            if(a%b==0){
+                cout << "YES\n";
+            }
+            else{
+                long long int rem = a%b;
+                if(b%rem==0){
+                    cout<<"YES\n";
+                }
+                else{
+                    cout << "NO\n";
+                }
+            }
+        }
     }
 }
